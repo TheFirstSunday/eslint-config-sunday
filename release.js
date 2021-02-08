5365117b62ffc81796d4b99bf61a7b4fdb2454e7
zhaonan <zhaonan@fiture.com>
2021-02-08 14:28:46 +0800

    chore(release): auto generate version

---
+++ release.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fs = require('fs')
+const { execSync } = require('child_process')
+
+function cmd(command) {
+  try {
+    const output = execSync(command)
+    return output.toString()
+  } catch (error) {
+    return ''
+  }
+}
+
+function getVersion() {
+  try {
+    const tag = cmd('git describe --tags')
+      .split('-')
+      .filter(Boolean)[0].trim()
+
+    if (tag && /^v\d+.\d+.\d+$/.test(tag)) {
+      return tag.slice(1)
+    }
+
+    return ''
+  } catch (error) {
+    return ''
+  }
+}
+
+const packageInfo = JSON.parse(fs.readFileSync('package.json'))
+const version = getVersion()
+
+if (version && version !== packageInfo.version) {
+  packageInfo.version = version
+  fs.writeFileSync('package.json', JSON.stringify(packageInfo, null, 2) + '\r')
+}
